f932bf105d870f515bdf282dd89a5b252702239c
max-in-to <contact@maxgardiner.ca>
2020-05-21 16:18:02 -0700

    update icon colors

--- src/app/places/place-summary/place-summary.component.ts
+++ src/app/places/place-summary/place-summary.component.ts
@@ -13,64 +13,71 @@ export class PlaceSummaryComponent implements OnInit {
 
   @Input() place: Place;
   is_favourite: boolean = false;
-  popularity: {level:number, text: string}|null = null;
+  popularity: { level: number, text: string } | null = null;
   constructor(private router: Router, private favouritePlacesServ: FavouritePlacesService, private authServ: AuthService) { }
 
   ngOnInit(): void {
-    let user =  this.authServ.getCurrentUser();
+    let user = this.authServ.getCurrentUser();
     this.is_favourite = user != null && user.favourite_places.indexOf(this.place.place_id) != -1;
 
-    if(this.place?.popularity_data?.popular_times){
-      let d = new Date();
-      let current_day = d.toLocaleString('en-us', {  weekday: 'long' });
-      let current_hour = d.getHours();
+    if (this.place?.popularity_data) {
+      if (this.place.popularity_data.popular_times) {
+        let d = new Date();
+        let current_day = d.toLocaleString('en-us', { weekday: 'long' });
+        let current_hour = d.getHours();
         let day_info = this.place.popularity_data.popular_times.filter(d => d.name == current_day);
-        this.popularity = {level: +day_info[0].data[current_hour], text: this.generatePopularityText(this.place.name, +day_info[0].data[current_hour])};
+        this.popularity = { level: this.place.popularity_data.current_popularity ? this.place.popularity_data.current_popularity :  +day_info[0].data[current_hour], text: this.generatePopularityText(this.place.name, +day_info[0].data[current_hour]) };
+      }
+      else if (this.place.popularity_data.current_popularity) {
+        this.popularity = { level: this.place.popularity_data.current_popularity, text: this.generatePopularityText(this.place.name,  this.place.popularity_data.current_popularity) };
+      
+      } 
+
     }
-     
-    
+
+
   }
 
-  private generatePopularityText(place_name: string, popularity_level: number): string{
+  private generatePopularityText(place_name: string, popularity_level: number): string {
 
-    if (popularity_level == 0){
+    if (popularity_level == 0) {
       //closed
       return 'closed.'
     }
-    else if (popularity_level > 0 && popularity_level < 40){
+    else if (popularity_level > 0 && popularity_level < 40) {
       return 'not busy right now.'
     }
-    else if (popularity_level >= 40 && popularity_level <= 60){
+    else if (popularity_level >= 40 && popularity_level <= 60) {
       return 'averagely busy right now.'
     }
-    else if (popularity_level > 60 && popularity_level <= 75){
+    else if (popularity_level > 60 && popularity_level <= 75) {
       return 'fairly busy right now.'
     }
-    else{
+    else {
       return 'very busy right now.'
     }
   }
 
-  favouritePlace(){
-      let user =  this.authServ.getCurrentUser();
-      if (user){
-        this.favouritePlacesServ.addPlaceToUserFavourites(this.place,user.auth_token).then(favourites => {
-        
-          this.is_favourite = true;
-        });
-      }
-      else{
-        this.router.navigate(['/login']);
-      }
-      
+  favouritePlace() {
+    let user = this.authServ.getCurrentUser();
+    if (user) {
+      this.favouritePlacesServ.addPlaceToUserFavourites(this.place, user.auth_token).then(favourites => {
+
+        this.is_favourite = true;
+      });
+    }
+    else {
+      this.router.navigate(['/login']);
+    }
+
   }
 
-  unfavouritePlace(){
-    let user =  this.authServ.getCurrentUser();
-    if (user){
+  unfavouritePlace() {
+    let user = this.authServ.getCurrentUser();
+    if (user) {
       this.favouritePlacesServ.removePlaceFromUserFavourites(this.place, user.auth_token).then(favourites => {
-      
-    this.is_favourite = false;
+
+        this.is_favourite = false;
       });
     }
 
